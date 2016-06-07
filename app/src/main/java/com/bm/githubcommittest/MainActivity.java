c87cdd4f47f5b691b4d914a0877574d1153cf0f8
yichangyanyu <674590667@qq.com>
2016-06-07 16:18:54 +0800

    更改mainactivity1

--- app/src/main/java/com/bm/githubcommittest/MainActivity.java
+++ app/src/main/java/com/bm/githubcommittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "11111", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "11111", Toast.LENGTH_SHORT).show();
     }
 }
